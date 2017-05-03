fe0304ee9c26ed08d38dd30c665e735f76bcb6f0
dotan raz <dotan@invoicecycle.com>
2017-05-03 12:43:13 +0300

    Modal Validations

--- MyFirstWebAPI/Controllers/TodoController.cs
+++ MyFirstWebAPI/Controllers/TodoController.cs
@@ -12,6 +12,7 @@ namespace MyFirstWebAPI.Controllers
     {
         // GET api/values
         [HttpGet]
+        [Produces(typeof(Todo))]
         public IEnumerable<Todo> Get()
         {
             return new List<Todo>() { new Todo() { Id = 1, Caption = "Some todo title", Note = "do something" }, new Todo() { Id = 2, Caption = "Some todo title 2", Note = "do something" } };
@@ -19,16 +20,25 @@ namespace MyFirstWebAPI.Controllers
 
         // GET api/values/5
         [HttpGet("{id}")]
+        [Produces("application/json", Type = typeof(Todo))]
         public Todo Get(int id)
         {
-            return new Todo() {Id = id, Caption = $"Some todo title {id}", Note = "do something"};
+            return new Todo() { Id = id, Caption = $"Some todo title {id}", Note = "do something" };
         }
 
         // POST api/values
         [HttpPost]
+        [Consumes("application/json")]
+        [Produces("application/json", Type = typeof(Todo))]
         public IActionResult Post([FromBody]Todo value)
         {
-            return CreatedAtAction("Get", new {id = value.Id}, value);
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+
+            return CreatedAtAction("Get", new { id = value.Id }, value);
         }
 
         // PUT api/values/5
--- MyFirstWebAPI/Models/Todo.cs
+++ MyFirstWebAPI/Models/Todo.cs
@@ -1,9 +1,14 @@
-﻿namespace MyFirstWebAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyFirstWebAPI.Models
 {
     public class Todo
     {
+        [Required]
         public int Id { get; set; }
+        [Required]
         public string Caption { get; set; }
         public string Note { get; set; }
+        public bool IsCompleted { get; set; }
     }
 }
